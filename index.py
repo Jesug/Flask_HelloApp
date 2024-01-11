affbe5413eeb245569672bc6bb7632832ee5e7e8
Ilori Jesugbemisola <39275223+Jesug@users.noreply.github.com>
2024-01-11 17:38:13 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template, request, flash
 
+app = Flask(__name__)
+app.secret_key = "xellux_abcdef"
+
+@app.route("/")
+def home():
+    flash("What is your name?")
+    return render_template("index.html")
+
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+    return render_template("index.html")
+    
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
